ca1d77cd48a25eea0589ed6c701abff427b02723
UEDavidH <109534357+UEDavidH@users.noreply.github.com>
2022-08-01 11:06:29 -0500

    Add files via upload

---
+++ css/scripts/main.js
@@ -0,0 +1,8 @@
+'use strict';
+const btn = document.getElementById('boton2');
+
+btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const form = document.getElementById('form');
+    console.log(form);
+});//2do parámetro función tipo flecha anónima
